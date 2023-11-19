a7ceca6f7f84937629014e00a32d55acc7764bce
Agm3002 <104405812+Agm3002@users.noreply.github.com>
2023-11-19 01:21:21 -0700

    Pantalla Emergente, boton activar/desactivar propiedad
    Se agrego boton Activar/Desactivar propiedad y se agrego la pantalla emergente para cuando tengo una solicitud de cambio al arrendador

---
+++ static/js/pantallaEmergente.js
@@ -0,0 +1,7 @@
+function mostrarPantallaEmergente() {
+    document.getElementById("miPantallaEmergente").style.display = "block";
+}
+
+function cerrarPantallaEmergente() {
+    document.getElementById("miPantallaEmergente").style.display = "none";
+}
