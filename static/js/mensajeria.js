acb499f18293d6bffff10d2a58b679ccf0c26889
daronslzr <daronslzr@gmail.com>
2023-11-22 01:40:29 -0700

    Diseño de mensajeria con arrendador

---
+++ static/js/mensajeria.js
@@ -0,0 +1,7 @@
+function hoverBoton() {
+    document.getElementById('iconoStroke').style.stroke = "#009ef9";
+}
+
+function unhoverBoton(){
+    document.getElementById('iconoStroke').style.stroke = "#38B6FF";
+}
